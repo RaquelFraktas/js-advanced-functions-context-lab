0cbb781030b8a0ca7aea0c9bd853dbec61678616
github username <github email address>
2021-08-30 20:42:29 -0400

    commit tree 7e6928dfc93184067c92155aeb4113ce1579dc30

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
 /* Your Code Here */
 
+function createEmployeeRecord(record){
+  return {
+      firstName: record[0],
+      familyName: record[1],
+      title: record[2],
+      payPerHour: record[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+  }
+}
+
+function createEmployeeRecords(records){
+  return records.map(record => createEmployeeRecord(record))
+}
+
+function createTimeInEvent(dateStamp){
+   let splitDate= dateStamp.split(" ")
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(splitDate[1]),
+        date: splitDate[0]
+    })
+    return this
+}
+
+function createTimeOutEvent (dateStamp){
+  let [date, hour]= dateStamp.split(' ')
+  this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date,
+  })
+  return this
+}
+
+function hoursWorkedOnDate(soughtDate){
+  let timeIn= this.timeInEvents.find(e => e.date === soughtDate)
+  let timeOut= this.timeOutEvents.find(e => e.date === soughtDate)
+  return (timeOut.hour-timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+   let hoursWorked= hoursWorkedOnDate.call(this, date)
+   let wages= this.payPerHour
+   return hoursWorked * wages
+}
+
+function calculatePayroll(eRecords){
+  return eRecords.reduce((acc, record) => acc +allWagesFor.call(record),0)
+}
+
+function findEmployeeByFirstName(srcArray, fName){
+  return srcArray.find( e => e.firstName === fName)
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,3 +75,4 @@ let allWagesFor = function () {
 
     return payable
 }
+
